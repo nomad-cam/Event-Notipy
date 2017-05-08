8189b035453abb1813b6f4cd7f9b5ac06b165d12
Cameron Rodda <cameron.rodda@synchrotron.org.au>
2017-05-08 12:28:34 +1000

    moved to gevent.pywsgi

--- run_EventNotipy.py
+++ run_EventNotipy.py
@@ -1,4 +1,13 @@
 from eventnotipy import app
+from gevent.pywsgi import WSGIServer
+import time
 
 app.debug = True # disable for production
-app.run(host='0.0.0.0',port=8540)
+
+# if __name__ == '__main__':
+# app.run(host='0.0.0.0',port=8540)
+# use WSGIServer to try and reduce timeout errors seen occasionally
+date = time.strftime('%Y-%m-%d %H:%M:%S')
+print('Server Started: %s' % date)
+print('Running on port 8540...')
+WSGIServer(('0.0.0.0',8540),app).serve_forever()
